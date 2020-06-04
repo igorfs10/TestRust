d00239075a78931ed19d9114b291d79d16310c84
Igorfs10 <igorfs10@gmail.com>
2020-06-04 00:41:30 -0300

    configuracao de execucao e add anotacoes e codigo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+// A aplicação inicia com a função main
 fn main(){
-    println! ("Ola Mundo");
+    const NOME: &str = "Igor";
+    const IDADE: u8 = 27;
+    // Função macro usa "!"
+    println! ("Olá, meu nome é {name} e tenho {age} anos.", name = NOME, age = IDADE);
+
+    let resultado = soma(5, 1);
+    println! ("A soma de 5 + 1 é {resultado}", resultado = resultado);
+
+    const CARA: Pessoa = Pessoa {nome: "João", idade: 55};
+    let dados_cara: Dados = Dados {altura: 1.75, peso: 88.0 };
+    println! ("O cara se chama {} e tem {} anos.", CARA.nome, CARA.idade);
+    println! ("Altura = {}", dados_cara.altura);
+    println! ("Peso = {}", dados_cara.peso);
 }
+
+/* Em rust você declara a função com fn + nome + parâmetros entre parenteses e
+    uma seta que informa o tipo do retorno.
+    Não é necessário usar o return, nesse caso ele retorna o valor da ultima linha sem ;
+*/
+fn soma(primeiro_numero: i64, segundo_numero: i64) -> i64 {
+    // return primeiro_numero + segundo_numero;
+    primeiro_numero + segundo_numero // Mais comum
+}
+
+
+// Struct é um tipo criado que pode armazenar variáveis com outros tipos definidos.
+struct Dados {
+    altura: f64,
+    peso: f64
+}
+
+// 'a é necessário para criar struct constante
+// ' é um alterador de tempo de vida da variável e são usados depois da anotação de ponteiro & com 1 caracter geralmente
+// 'static tem o maior tempo de vida, que é a aplicação inteira que é o mesmo da constante
+struct Pessoa<'a> {
+    nome: &'a str,
+    idade: u8
+}
+
+/*
+    let : Declare variable
+    const : Declare constant
+    fn : Declare function
+
+    bool : The boolean type.
+    char : A character type.
+    i8 : The 8-bit signed integer type.
+    i16 : The 16-bit signed integer type.
+    i32 : The 32-bit signed integer type.
+    i64 : The 64-bit signed integer type.
+    i128 : The 128-bit signed integer type.
+    isize : The pointer-sized signed integer type.
+    u8 : The 8-bit unsigned integer type.
+    u16 : The 16-bit unsigned integer type.
+    u32 : The 32-bit unsigned integer type.
+    u64 : The 64-bit unsigned integer type.
+    u128 : The 128-bit unsigned integer type.
+    usize : The pointer-sized unsigned integer type.
+    f32 : The 32-bit floating point type.
+    f64 : The 64-bit floating point type.
+    array : A fixed-size array, denoted [T; N], for the element type, T, and the non-negative compile-time constant size, N.
+    slice : A dynamically-sized view into a contiguous sequence, [T].
+    str : String slices.
+    tuple : A finite heterogeneous sequence, (T, U, ..).
+
+    Decimal : 98_222
+    Hex	: 0xff
+    Octal : 0o77
+    Binary : 0b1111_0000
+    Byte (u8 only) : b'A'
+*/
